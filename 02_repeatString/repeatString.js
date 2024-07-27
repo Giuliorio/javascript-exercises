0f8b284400c037531ff926582fb7869b9b8515c3
Giulio Riondino <riondino.giulio@gmail.com>
2024-07-27 09:52:25 -0700

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
 
+    let value = '';
+    for (let i = 0; i < num; i++) {
+        value += string;
+    }
+    return value
 };
 
 // Do not edit below this line
