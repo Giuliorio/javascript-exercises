0a9e2b8b1135398fe9a3b575604d5d48a684cbc2
Giulio Riondino <riondino.giulio@gmail.com>
2024-08-04 16:37:56 -0700

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((tot, currValue) => tot + currValue, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((tot, currValue) => tot * currValue)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+ if (num < 2) return 1
+  let total = 1
+  for (let i = num; i > 1; i--) {
+    total *= i
+  }
+return total
+ 
 };
 
 // Do not edit below this line
