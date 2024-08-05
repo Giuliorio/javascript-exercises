61f07a7e2793cc4ff9f5b45a2614134f916d9c3b
Giulio Riondino <riondino.giulio@gmail.com>
2024-08-04 19:09:07 -0700

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n)
+    if (n === 0) return 0
+    if (n < 0) return 'OOPS'
+    const sequence = [1, 1]
+    if (sequence.len === n - 1) return sequence[n - 1]
+    for (let i = 0; i < n - 1; i++) {
+        const len = sequence.length
+        sequence.push(sequence[len - 1] + sequence[len - 2])
+    }
+    return sequence[n - 1]
 };
 
 // Do not edit below this line
