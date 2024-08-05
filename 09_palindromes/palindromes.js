e9cf6b5a88b88434f4e62ec2543456304e2be2ab
Giulio Riondino <riondino.giulio@gmail.com>
2024-08-04 17:56:37 -0700

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphabet = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+    const cleanedString = [...word.toLowerCase()]
+        .filter(char => alphabet.includes(char))
+        .join('')
+    const reversed = [...cleanedString].reverse().join('')
 
-};
-
+    return cleanedString === reversed
+}
 // Do not edit below this line
 module.exports = palindromes;
