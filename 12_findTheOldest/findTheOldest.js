c3d738956fd76d5ce985e8d095d6b8bf89a2d845
Giulio Riondino <riondino.giulio@gmail.com>
2024-08-04 19:20:42 -0700

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const getAge = (person) => {
+        const minuend = person.yearOfDeath 
+        ? person.yearOfDeath 
+        : new Date().getFullYear()
 
+        return minuend - person.yearOfBirth
+    }
+    const peopleByAge = people.sort((a, b) => {
+        return getAge(b) - getAge(a)
+    })
+
+    return peopleByAge[0]
 };
 
 // Do not edit below this line
